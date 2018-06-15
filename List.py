79fd8e7a6f7bec66db21e3d52be49415b4fc81bc
chirag.mangukiya@gmail.com <nokia76107610>
2018-06-15 17:02:20 +0530

    Misc

--- List.py
+++ List.py
@@ -1,26 +1,64 @@
+import selenium
 import MyMethods
 import time
 
-driver = MyMethods.invoke_app()
+
+driver = MyMethods.invoke_app("en", "in")
+
+def check_exist_by_id(id):
+    try:
+        driver.find_element_by_id(id)
+    except selenium.common.exceptions.NoSuchElementException:
+        return False
+    return True
+
+if check_exist_by_id('com.lge.eula:id/btn_agree'):
+    driver.find_element_by_id('com.lge.eula:id/btn_agree').click()
+
+time.sleep(2)
 
 driver.find_element_by_id('com.android.packageinstaller:id/permission_allow_button').click()
 time.sleep(1)
 driver.find_element_by_id('com.android.packageinstaller:id/permission_allow_button').click()
 time.sleep(1)
+
+if check_exist_by_id('com.lge.hifirecorder:id/tip_help_next_button'):
+    driver.find_element_by_id('com.lge.hifirecorder:id/tip_help_next_button').click()
+    time.sleep(1)
+    driver.find_element_by_id('com.lge.hifirecorder:id/tip_help_previous_button').click()
+    time.sleep(1)
+    driver.find_element_by_id('com.lge.hifirecorder:id/tip_help_next_button').click()
+    time.sleep(1)
+    driver.find_element_by_id('com.lge.hifirecorder:id/tip_help_next_button').click()
+    time.sleep(1)
+    driver.find_element_by_id('com.lge.hifirecorder:id/tip_help_next_button').click()
+    time.sleep(1)
+    driver.find_element_by_id('com.lge.hifirecorder:id/tip_help_next_button').click()
+    time.sleep(1)
 
 driver.find_element_by_xpath("//android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/"
                              "android.widget.LinearLayout/android.widget.TextView").click()
 
-#myCount = driver.find_element_by_xpath("//android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/"
-#                            "android.widget.LinearLayout/android.widget.LinearLayout/android.widget.RelativeLayout/"
-#                            "android.widget.ExpandableListView/android.widget.LinearLayout/android.widget.LinearLayout").size()
+#cou = driver.find_elements_by_android_uiautomator('new UiScrollable(new UiSelector()).scrollIntoView(className("android.widget.FrameLayout"))')
 
-myCount = driver.find_element_by_id('com.lge.hifirecorder:id/list_item_layout').click()
-driver.find_element_by_id('com.lge.hifirecorder:id/closebutton').click()
-driver.find_element_by_id('com.lge.hifirecorder:id/list_item_layout').click()
-driver.find_element_by_id('com.lge.hifirecorder:id/playButton').click()
-driver.find_element_by_id('com.lge.hifirecorder:id/playButton').click()
-driver.find_element_by_id('com.lge.hifirecorder:id/deletelayout').click()
-driver.find_element_by_id('android:id/button2').click()
-driver.find_element_by_id('com.lge.hifirecorder:id/deletelayout').click()
-driver.find_element_by_id('android:id/button1').click()
+if check_exist_by_id('com.lge.hifirecorder:id/list_item_layout'):
+    driver.find_element_by_id('com.lge.hifirecorder:id/list_item_layout').click()
+    driver.find_element_by_id('com.lge.hifirecorder:id/closebutton').click()
+    driver.find_element_by_id('com.lge.hifirecorder:id/list_item_layout').click()
+    driver.find_element_by_id('com.lge.hifirecorder:id/playButton').click()
+    if check_exist_by_id('com.lge.hifirecorder:id/playButton'):
+        driver.find_element_by_id('com.lge.hifirecorder:id/playButton').click()
+        driver.find_element_by_id('com.lge.hifirecorder:id/deletelayout').click()
+        driver.find_element_by_id('android:id/button2').click()
+        driver.find_element_by_id('com.lge.hifirecorder:id/deletelayout').click()
+        driver.find_element_by_id('android:id/button1').click()
+
+driver.find_element_by_xpath("//android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/"
+                             "android.widget.LinearLayout/android.widget.TextView").click()
+
+def record_voice(duration):
+    driver.find_element_by_id('com.lge.hifirecorder:id/recordButtonLayout').click()
+    time.sleep(int(duration))
+
+
+record_voice(5)
--- MyMethods.py
+++ MyMethods.py
@@ -7,10 +7,10 @@ def invoke_app(lan, locale):
         "deviceName": "LMG710EM462f05f5", #Device Id
         "language": lan, #Langauge Code
         "locale": locale, #Country Code
-        #"appPackage": "com.lge.hifirecorder",
-        #"appActivity": "com.lge.hifirecorder.LaunchHifiRecorder",
-        "appPackage": "com.android.settings",
-        "appActivity": "com.android.settings.Settings",
+        "appPackage": "com.lge.hifirecorder",
+        "appActivity": "com.lge.hifirecorder.LaunchHifiRecorder",
+        #"appPackage": "com.android.settings",
+        #"appActivity": "com.android.settings.Settings",
         "platformName": "Android"
     }
     return webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
