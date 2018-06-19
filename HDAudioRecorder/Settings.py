eb334cd37d4e20f1c4187f68bcd91655f547b49c
chirag.mangukiya@gmail.com <nokia76107610>
2018-06-19 14:28:12 +0530

    Added elements in Settings.py

--- HDAudioRecorder/Settings.py
+++ HDAudioRecorder/Settings.py
@@ -3,21 +3,31 @@ import time
 
 driver = MyMethods.invoke_app("en", "uk")
 
-driver.find_element_by_xpath("//android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/"
-                                     "android.view.ViewGroup/android.widget.ImageButton").click()
-time.sleep(2)
-driver.find_element_by_xpath("//android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/"
-                                     "android.support.v4.widget.DrawerLayout/android.widget.ListView/android.widget.LinearLayout[3]/"
-                                     "android.widget.LinearLayout").click()
-time.sleep(2)
-
-driver.find_element_by_xpath("//android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/"
-                             "android.view.ViewGroup/android.widget.LinearLayout/android.widget.ImageButton").click()
-
-driver.find_element_by_xpath("//android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/"
-                             "android.widget.LinearLayout[3]").click()
 
 class SettingsOperations:
 
+    def open_normal_mode(self):
+        driver.find_element_by_xpath("//android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/"
+                                     "android.view.ViewGroup/android.widget.ImageButton").click()
+        time.sleep(2)
+        driver.find_element_by_xpath("//android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/"
+                                     "android.support.v4.widget.DrawerLayout/android.widget.ListView/"
+                                     "android.widget.LinearLayout[1]/android.widget.LinearLayout").click()
+        time.sleep(2)
+
     def open_settings_normal_mode(self):
-        
+        driver.find_element_by_xpath("//android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/"
+                                     "android.view.ViewGroup/android.widget.LinearLayout/android.widget.ImageButton").click()
+        time.sleep(2)
+        driver.find_element_by_xpath("//android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/"
+                                     "android.widget.LinearLayout[3]").click()
+        time.sleep(2)
+        driver.find_element_by_xpath("//android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/"
+                                     "android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ListView/"
+                                     "android.widget.FrameLayout/android.widget.LinearLayout").click()
+
+temp = SettingsOperations()
+temp.open_normal_mode()
+temp.open_settings_normal_mode()
+
+
