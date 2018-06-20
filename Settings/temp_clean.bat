6f4a5da6c9a5bf6a686f040d77b375bd9b954456
chirag.mangukiya@gmail.com <nokia76107610>
2018-06-20 15:02:24 +0530

    Misc

--- Settings/temp_clean.bat
+++ Settings/temp_clean.bat
@@ -1,4 +1,4 @@
-echo y | del C:\Users\chirag.mangukiya\AppData\Local\Temp\*.* /S
-rd C:\Users\chirag.mangukiya\AppData\Local\Temp\ /s /q
-md C:\Users\chirag.mangukiya\AppData\Local\Temp
-Cacls C:\Users\chirag.mangukiya\AppData\Local\Temp /E /P EveryOne:F
+echo y | del C:\Users\chirag.mangukia\AppData\Local\Temp\*.* /S
+rd C:\Users\chirag.mangukia\AppData\Local\Temp\ /s /q
+md C:\Users\chirag.mangukia\AppData\Local\Temp
+Cacls C:\Users\chirag.mangukia\AppData\Local\Temp /E /P EveryOne:F
